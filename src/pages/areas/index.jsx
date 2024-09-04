a1ffce8588a6da9759f2b73ca8b1ef0465c9cd28
Felipe da Silva <silvaafelipe016@gmail.com>
2024-09-04 18:28:13 -0300

    feat(pages): add areas page

---
+++ src/pages/areas/index.jsx
@@ -0,0 +1,34 @@
+import { areas } from "../../utils/areas";
+
+import { PrimaryButton } from "../../components/Button";
+
+import "./style.css";
+
+function Areas() {
+  return (
+    <section className="areas-container">
+      <div className="content">
+        <div className="text-container">
+          <h2>Atuações na cv9 projetos</h2>
+          <h3>
+            Acesse as mais avançadas soluções de construção, com atendimento
+            dedicado e execução de excelência para transformar suas ideias em
+            obras concretas.
+          </h3>
+        </div>
+
+        <div className="cards-container">
+          {areas.map((area) => (
+            <div className="card">
+              <img src={area.image} alt="" />
+              <h3>{area.title}</h3>
+            </div>
+          ))}
+        </div>
+        <PrimaryButton>Faça um orçamento</PrimaryButton>
+      </div>
+    </section>
+  );
+}
+
+export default Areas;
